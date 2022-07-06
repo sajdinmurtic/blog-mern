9f18b13fb87b335b3087863eea0a9f15e22fc639
Sajdin Murtić <sajdin_m@hotmail.com>
2022-07-06 08:18:47 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3000, ()=>{
+    console.log('server is running on PORT 3000')
+})
